3d6b649db1f48901552df904bba61a4a87721227
R1d3r90 <hubertlion@aol.com>
2024-05-03 12:24:05 +0200

    add Stream

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main {
+    public static void main (String[] args) {
+        List<Integer> numbers = Arrays.asList(2, 1, 7,0, 4, 5, 9, 7, 3, 8, 6);
+        List<Integer> numbersStreamed = Collections.singletonList(numbers.stream()
+                .filter(integer -> integer % 2 == 0)
+                .map(integer -> integer * 2)
+                .sorted()
+                .reduce(0, (a, b) -> a + b));
+
+        numbersStreamed.forEach(System.out::println);
+    }
 }
